4b277827f2e298f341362a94b34a07a702f1ebb6
mallamace <nathan@cruzeo.com>
2020-05-03 00:40:34 -0700

    Updating File

--- src/main/java/com/rosebunnygamer/newtestapp/Main.java
+++ src/main/java/com/rosebunnygamer/newtestapp/Main.java
@@ -4,6 +4,13 @@ public class Main {
 
 
     public static void Main(String[] args){
+
+        //Client Builder Example from https://twitch4j.gitlab.io/twitch4j/getting-started/client-builder/
+         
+        TwitchClient twitchClient = TwitchClientBuilder.builder()
+                .withEventManager(eventManager)
+                .build();
+
         System.out.println("OK OK OK OK ");
     }
 }
