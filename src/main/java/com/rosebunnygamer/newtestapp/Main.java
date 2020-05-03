92235f8bb165c19859ed952a02cb09f29482b7bb
mallamace <nathan@cruzeo.com>
2020-05-03 00:53:05 -0700

    Minor Changes to gradle and Main JAVA Class

--- src/main/java/com/rosebunnygamer/newtestapp/Main.java
+++ src/main/java/com/rosebunnygamer/newtestapp/Main.java
@@ -1,12 +1,17 @@
 package com.rosebunnygamer.newtestapp;
 
+
+
+import com.rosebunnygamer.TwitchClient;
+import com.rosebunnygamer.TwitchClientBuilder;
+
 public class Main {
 
 
     public static void Main(String[] args){
 
         //Client Builder Example from https://twitch4j.gitlab.io/twitch4j/getting-started/client-builder/
-         
+
         TwitchClient twitchClient = TwitchClientBuilder.builder()
                 .withEventManager(eventManager)
                 .build();
